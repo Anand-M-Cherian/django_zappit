64d3bd1db5f0f01f3b5736ce593baa216e804cf2
Anand <anandcherian.585@gmail.com>
2024-11-23 15:41:35 +0530

    initial commit

---
+++ posts/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Post, Vote
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    author_id = serializers.ReadOnlyField(source='author.id')
+    votes = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'author', 'author_id', 'created_on', 'votes']
+
+    def get_votes(self, post):
+        return Vote.objects.filter(post=post).count()
+
+class VoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Vote
+        fields = ['id']
